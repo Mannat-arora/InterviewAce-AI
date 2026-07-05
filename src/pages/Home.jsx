f05615970f01a0c1c4693b6de3e94805ec1c34f3
Mannat-arora <mannat240024@gmail.com>
2026-07-05 17:59:21 +0530

    Initial React project setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import Navbar from "../components/Navbar";
+function Home() {
+    return (
+       <div>
+        <Navbar />
+    <h1>InterviewAce AI</h1>
+
+    <h3>Your Personal Placement Companion</h3>
+
+    <p>
+        Prepare for Coding Interviews,<br />
+        Mock HR Interviews,<br />
+        Resume Analysis,<br />
+        Company-wise Preparation
+    </p>
+</div>
+    );
+}
+
+export default Home;
